2f0704028dd8bf6ab49870ec8519e002940a92d7
Eliot Lear <lear@cisco.com>
2016-11-11 17:03:14 +0100

    LICENSE and mud maker

---
+++ mudmaker/aux.js
@@ -0,0 +1,109 @@
+// This code adopted from Allen Liu on Random Snippets
+// http://www.randomsnippets.com/2008/02/21/how-to-dynamically-add-form-elements-via-javascript/
+ 
+var counter = [ 1 , 1 , 1, 1, 1, 1, 1, 1];
+
+var nref = [ 0, 0, 0, 0 ];
+
+var limit = 10;
+
+function removeIt(elemId) {
+    var elem=document.getElementById(elemId);
+    elem.parentNode.removeChild(elem);
+}
+
+    
+
+function addInput(divName,names,dnsorurl,proto,port,i){
+
+    if (counter[i] == limit)  {
+        alert("You have gotten silly: you have reached the limit of adding "
+	      + counter[i] + " inputs");
+    }
+    else {
+        var newdiv= document.createElement('span');;
+	var minus=document.createElement('input');
+	var typefield;
+	var pattern;
+	var portdivname;
+	var seldivname;
+	var onchange;
+	var hidden;
+	var any;
+	var fieldinfo;
+	var divid;
+
+	divid= divName + i + "-" +  counter[i];
+	newdiv.id=divid;
+	minus.setAttribute("type","Button");
+	minus.setAttribute("onClick","removeIt('" + divid + "');");
+	minus.value="-";
+
+	if ( dnsorurl == 'dns' ) {
+	    typefield="'text'";
+	    pattern = " pattern='[a-z0-9.-]+\.[a-z]{2,3}$'";
+	} else {
+	    typefield="'url'";
+	    pattern = "";
+	}
+
+	portdivname= divid + 'portdiv' + counter[i];
+	selname= divid + 'sel' + counter[i];
+
+	if ( divName == 'locoutlist' || divName == 'locinlist' ) {
+	    onchange=' ';
+	    hidden ="' style='visibility: inherit' ";
+	    any = '';
+	    pattern = " ";
+	    fieldinfo = 'readonly="" value="any" ';
+	} else {
+	    onchange=
+		" onchange=\"tcporudp('" + selname + "','" + portdivname + "');\"";
+	    hidden="' style='visibility: hidden'";
+	    any = "<option value='any'>Any</option>";
+	    fieldinfo="maxlength='120'";
+	}
+
+        newdiv.innerHTML = 
+            " <br><input type=" + typefield + "name='" + names  + "'" + pattern +
+	    " size='40' " + fieldinfo + ">&nbsp;&nbsp;&nbsp;" +
+	    " Protocol&nbsp;&nbsp;<select id='" + selname + "' name='" + proto + "'" +
+	    onchange + ">" +
+	    any +
+	    "<option value='tcp'>TCP</option>" +
+	    "<option value='udp'>UDP</option>" +
+	    "</select><span id='" + portdivname + hidden + ">"
+	    + "&nbsp;&nbsp;&nbsp;" + 
+	    "Port&nbsp; <input max='65535' min='0' name='" +
+	    port + "' style='width:60px' type='number'></span>";
+	    
+
+        document.getElementById(divName).appendChild(newdiv);
+	newdiv.appendChild(minus);
+	
+        counter[i]++;
+    }
+}
+
+function tcporudp(selectid,portid) {
+    if (document.getElementById(selectid).value == 'any') {
+	document.getElementById(portid).style.visibility='hidden';
+    } else {
+	document.getElementById(portid).style.visibility='inherit';
+    }
+}
+
+function yesnoCheck(one2check, outer,inner,refind) {
+    if (document.getElementById(one2check).checked ) {
+        document.getElementById(outer).style.display = 'block';
+        document.getElementById(inner).style.display = 'block';
+	nref[refind]++;
+    } else {
+        document.getElementById(inner).style.display = 'none';
+	
+	nref[refind]--;
+        if ( nref[refind] < 1 ) {
+            document.getElementById(outer).style.display = 'none';
+        }
+    }
+}
---
+++ mudmaker/makemud2.php
@@ -0,0 +1,465 @@
+<?php
+/* Copyright (c) 2016, Cisco Systems
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*
+ * Take a few inputs and generate a MUD file.
+ *
+ * All components are optional.
+ * We will build two access lists: inbound and outbound.
+ * There are two directions in which communications can be initiated: to device
+ * and from device.  We allow the specified port outbound as a destination port
+ * and inbound as a source port when the device initiates.  We allow the specified
+ * port inbound as a destination port and outbound as a source port when others
+ * initiate.  
+ */
+
+use \DateTime;
+
+$aclhead= <<< ACL_HEAD
+"ietf-acl:access-lists" : {
+  "ietf-acl:access-list" : 
+  
+ACL_HEAD;
+  
+
+
+/* Rather than try to pretty print the json throughout, I have 
+ * borrowed some code from Kendall Hopkins and George Garchagudashvili
+ * from stackoverflow at the following URL:
+ *  http://stackoverflow.com/questions/6054033/pretty-printing-json-with-php
+ *
+ * Yes, this means that [null] looks a little weird.
+ */
+
+
+
+function prettyPrint( $json )
+{
+  $result = '';
+  $level = 0;
+  $in_quotes = false;
+  $in_escape = false;
+  $ends_line_level = NULL;
+  $json_length = strlen( $json );
+
+  for( $i = 0; $i < $json_length; $i++ ) {
+    $char = $json[$i];
+    $new_line_level = NULL;
+    $post = "";
+    
+    if( $ends_line_level !== NULL ) {
+      $new_line_level = $ends_line_level;
+      $ends_line_level = NULL;
+    }
+    
+    if ( $in_escape ) {
+      $in_escape = false;
+    }
+    else if( $char === '"' ) {
+      $in_quotes = !$in_quotes;
+    }
+    else if( ! $in_quotes ) {
+      switch( $char ) {
+      case '}': case ']':
+	$level--;
+	$ends_line_level = NULL;
+	$new_line_level = $level;
+	break;
+
+      case '{': case '[':
+	$level++;
+	
+      case ',':
+	$ends_line_level = $level;
+	break;
+
+      case ':':
+	$post = " ";
+	break;
+	
+      case " ": case "\t": case "\n": case "\r":
+	$char = "";
+	$ends_line_level = $new_line_level;
+	$new_line_level = NULL;
+	break;
+      }
+    }
+    else if ( $char === '\\' ) {
+      $in_escape = true;
+    }
+    
+    if( $new_line_level !== NULL ) {
+      $result .= "\n".str_repeat( "  ", $new_line_level );
+    }
+    $result .= $char.$post;
+  }
+
+  return $result;
+  
+}
+
+
+
+define(IS_LOCAL,1);
+define(IS_MFG, 2);
+define(IS_CONTROLLER, 3);
+define(IS_CLOUD, 4);
+
+
+// add a line to an acl.  
+
+$gotin = 0;
+$gotout = 0;
+$fail=0;	/* set if someone is screwing with us */
+
+
+function addace($acename, $direction, $target, $proto, $port, $type,$direction) {
+  
+  $ace="   {\n   " . '"rule-name" :' . '"' . $acename . '"' . ",\n" .
+    '   "matches" : {';
+
+  $ace = $ace . '"ietf-mud:direction-initiated": "' . $direction . '",' . "\n";
+  
+  if ( $direction == "to-device" ) {
+      $protmfrag = '"source-port-range"';
+    } else {
+    $protmfrag = '"destination-port-range"';
+  }
+  
+  if ( strlen($target) > 120 ) {
+    $fail=1;
+    return('');
+  }
+
+  switch ($type) {
+    case IS_LOCAL:
+      $ace = $ace . '  "ietf-mud:local-networks" : [ null ]';
+      break;
+    case IS_CONTROLLER:
+      if ( ! preg_match("^http[s]?://[a-z0-9.-]+\.[a-z]{2,3}/[a-z0-9()+,\-.:=@;$_!*'%/?#]+$^",$target) && 
+	   ! preg_match ("^urn:[a-z0-9][a-z0-9-]{0,31}:[a-z0-9()+,\-.:=@;$_!*'%/?#]+$^", $target)) {
+	$fail=1;
+	return('');
+      }
+      $ace = $ace . '      "ietf-mud:controller" : "' . $target . '"';
+      break;
+    case IS_CLOUD:
+      if ( $direction == "to-device" ) {
+	$ace = $ace . '"ietf-acldns:src-dnsname": "';
+      } else {
+	$ace = $ace . '"ietf-acldns:dst-dnsname": "';
+      }
+      $ace = $ace . $target . '"'; /* add host to ACE */
+      break;
+    case IS_MFG:
+      if ( ! preg_match('/[a-z0-9.-]+\.[a-z]{2,3}$/',$target)) {
+        $fail=1;
+        return('');
+      }
+      $ace = $ace . '      "ietf-mud:manufacturer" : "' . $target . '"';
+      break;
+  }
+  
+    /* in both cases we left off the comma because we don't know if there
+     * is another line, so... */
+
+    if ( $proto != 'any' ) {
+      if ( $proto == 'tcp' ) {
+	$np = '6';
+      } else {
+	if ( $proto == 'udp') {
+	  $np = '17';
+	}
+	else {
+	  $fail=1;
+	  return('');
+	  
+	}
+      }
+      $protbfrag = ",\n" . ' "protocol" : ' . $np . ",\n";
+      $protefrag = " : {\n" . '"lower-port" : ' . $port . ",\n" .
+	'      "upper-port" : ' . $port . "\n }";
+      
+      $ace = $ace . $protbfrag . $protmfrag . $protefrag;
+    }
+    
+
+    /* now close off matches, add action to the ACE and return it. */
+    
+    $ace = $ace . "\n }, " . '"actions" : {' . "\n " .
+      '"permit" : [null]' . "\n  }\n   }\n";
+    return($ace);
+}
+               
+
+
+function buildacegroup(&$target, &$proto, &$portarray, $namehead,$type,$direction) 
+{
+  global $inbound, $outbound, $gotin, $gotout;
+
+  if ( $gotin > 0 ) {
+    $inbound = $inbound . ",";
+  } else {
+    $gotin=1;
+  }
+  
+  if ( $gotout > 0) {
+    $outbound = $outbound . ",";
+  } else {
+    $gotout = 1;
+  }
+
+  // loop through all entries in array
+  for ($i = 0; isset($proto[$i]); $i++)
+    {
+      // for each outbound connection build two ACEs, one for the inbound ACL and
+      // one for the outbound ACL.
+
+      if ( $target[$i] == '' ) { // there may be no there there, especially on 0.
+	continue;
+      }
+      
+      if ( $i > 0 ) {
+	$outbound = $outbound . "  ,\n";
+	$inbound = $inbound . "  ,\n";
+      }
+
+      if ( $proto[$i] == 'any' ) {
+	$port = FALSE;
+      } else {
+	if ( $proto[$i] == 'udp' || $proto[$i] == 'tcp') {
+	  $port = $portarray[$i];
+	  if (! is_numeric($port) ) {
+	    $fail=1;
+	    return;
+	  }
+	  if ( $port < 0 || $port > 65535 ) {
+	    $fail=1;
+	    return;
+	  }
+	}
+	else {
+	  $fail=1;
+	  return;
+	}
+      }
+      
+      $s1= $namehead  . $i . "-out" ;
+      $s2= $namehead . $i . "-in";
+      
+      /* a little kludge here.  if we are dealing with local networks
+       * then $target is = FALSE.
+       */
+
+      if ( $type == IS_LOCAL ) {
+	$t2 = 'local';
+      } else {
+	$t2= $target[$i];
+      }
+      
+      
+      
+      $outbound = $outbound . addace($s1,"from-device", 
+				     $t2, $proto[$i],
+				     $port, $type,$direction);
+      $inbound = $inbound . addace($s2,"to-device",
+				     $t2, $proto[$i],
+				   $port, $type,$direction);
+    }
+}
+
+
+$inbound="";
+$outbound="";
+  
+  
+// We start by processing outbound cloud communications
+
+if ( isset($_POST['cloutbox'] ) ) {
+  // build based on cloud outbound
+
+  if (isset($_POST['clportout']))  { // distinctly possible user didn't enter ports
+    $clportout= $_POST['clportout'];
+  } else {
+    $clportout= FALSE;
+  }
+  
+  buildacegroup($_POST['clnamesout'],$_POST['clprotoout'],$clportout,
+		"clout",IS_CLOUD,"from-device");
+}
+
+
+// Next cloud inbound communications
+
+if ( isset($_POST['clinbox'] )) {
+  // build based on cloud inbound
+
+  if (isset($_POST['clportin'])) {
+    $clportin =  $_POST['clportin'];
+  } 
+  else {
+    $clportin = FALSE;
+  }
+
+  buildacegroup($_POST['clnamesin'],$_POST['clprotoin'],$clportin,
+		"clin",IS_CLOUD,"to-device");
+}
+
+
+
+// Next enterprise outbound
+
+if ( isset($_POST['entoutbox'] )) {
+  // build based on enterprise outbound
+  
+  if (isset($_POST['entportout']))  {
+    // distinctly possible user didn't enter ports   
+    $entportout= $_POST['entportout'];
+    
+  } else {
+    $entportout= FALSE;
+  }
+  
+  buildacegroup($_POST['entctrlout'],$_POST['entprotoout'],$entportout,
+		"entout",IS_CONTROLLER,"from-device");
+}
+
+// enterprise inbound
+if ( isset($_POST['entinbox'] )) {
+  // build based on enterprise inbound
+  if (isset($_POST['entportin']))  {
+    $entportout= $_POST['entportin'];    
+  }
+  else {
+    $entportout= FALSE;
+  }
+
+  buildacegroup($_POST['entctrlin'],$_POST['entprotoin'],$entportout,
+		"entin",IS_CONTROLLER,"to-device");
+}
+
+// local services
+
+if ( isset($_POST['localoutbox'])) {
+  // build local outbound services.
+  buildacegroup($_POST['localout'], $_POST['locprotoout'], $_POST['locportout'],
+		"locout",IS_LOCAL,"from-device");
+}
+
+if ( isset($_POST['localinbox'])) {
+  // build local inbound services.
+  buildacegroup($_POST['localin'], $_POST['locprotoin'], $_POST['locportin'],
+		"locin",IS_LOCAL,"to-device");
+}
+
+// Same manufacturer
+
+if ( isset($_POST['maninbox'])) {
+  // build local inbound services.
+  buildacegroup($_POST['mannamesin'], $_POST['manprotoin'], $_POST['manportin'],
+		"manin",IS_MFG,"to-device");
+}
+if ( isset($_POST['manoutbox'])) {
+  // build local inbound services.
+  buildacegroup($_POST['mannamesout'], $_POST['manprotoout'], $_POST['manportout'],
+		"manout",IS_MFG,"from-device");
+}
+
+
+  $choice=$_POST['ipchoice'];
+  if ( $choice != 'ipv4' && $choice != 'ipv6' && $choice != 'both' ) {
+    $fail=1;
+  }
+  
+if ( $fail ) {
+  exit;
+}
+
+
+
+  
+  
+if ( $gotin > 0 || $gotout > 0 ) {
+  $d=new Datetime('NOW');
+  $time=$d->format(DATE_RFC3339);
+
+  $supportInfo = '"ietf-mud:meta-info": { "lastUpdate" : "' . $time . '",' . "\n" .
+  '"systeminfo": "' . $_POST['sysDescr'] . '",' . "\n" .
+  '"cacheValidity" : 1440 },';
+  $output = "{\n". $supportInfo . "\n" . $aclhead;
+
+
+  $mudname="mud-" . rand(10000,99999) . "-";
+
+  if ( $choice == "ipv4" || $choice == "both" ) {
+  $ipv4inbound = '[ { "acl-name" : "' . $mudname . "v4in" . '",' . "\n" .
+    '"acl-type" : "ipv4-acl",' . "\n" .
+    '"ietf-mud:packet-direction" : "to-device",' .
+    "\n" . '"access-list-entries" : {' . '"ace" : [';
+  $ipv4outbound = ' { "acl-name" : "' . $mudname . "v4out" . '",' . "\n" .
+     '"acl-type" : "ipv4-acl",' . "\n" .
+     '"ietf-mud:packet-direction" : "from-device",' .
+     "\n" . '"access-list-entries" : {'  . '"ace" : [';
+  $ipv4inbound= $ipv4inbound . $inbound . " ]}},\n";
+  $ipv4outbound= $ipv4outbound . $inbound . " ]}}\n";
+  $output = $output . $ipv4inbound . $ipv4outbound;
+  
+    }
+
+  if ( $choice == "ipv6" || $choice == "both" ) {
+    $ipv6inbound = '[ { "acl-name" : "' . $mudname . "v6in" . '",' . "\n" .
+       '"acl-type" : "ipv6-acl",' . "\n" .
+       '"ietf-mud:packet-direction" : "to-device",' .
+       "\n" . '"access-list-entries" : {' . '"ace" : [';
+    $ipv6outbound = ' { "acl-name" : "' . $mudname . "v6out" . '",' . "\n" .
+      '"acl-type" : "ipv6-acl",' . "\n" .
+      '"ietf-mud:packet-direction" : "from-device",' .
+      "\n" . '"access-list-entries" : {' . '"ace" : [';
+    $ipv6inbound= $ipv6inbound . $inbound . " ]}},\n";
+    $ipv6outbound= $ipv6outbound . $inbound . "]}}\n";
+
+    if ( $choice == 'both' ) {
+      $output = $output . ",";
+    }
+    $output = $output . $ipv6inbound . $ipv6outbound;
+  }
+  
+  $output = $output . "]}}";
+  $output= prettyPrint($output);
+
+  print "<h1>Your MUD file is ready!</h1>";
+  print "<p>Congratulations!  You've just created your first MUD file.  Simply ";
+  print "Cut and paste beween the lines and stick into a file.  Your next steps ";
+  print "are to sign the file and place it in the location that its corresponding ";
+  print "MUD URL will find.</p>";
+  print "<hr>\n";
+  print "<pre>" . htmlentities($output) . "</pre>";
+  print "<hr>\n";
+} else {
+  
+  print "<h1>No output selected.  Click back and try again</h1>";
+}

3a6b64e319644a8fd3c2dbdeba5e420cccc54e04
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-07-20 05:11:52 -0400

    several bug fix and UI improvement

--- mud_visualizer/main.js
+++ mud_visualizer/main.js
@@ -8,6 +8,7 @@ const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow; 
 let addWindow; 
+var json_data_loaded = false;
 // Listen for app to be ready 
 app.on('ready',function(){
     //create new window 
@@ -16,7 +17,12 @@ app.on('ready',function(){
             nodeIntegration: true
         }
     });
-    mainWindow.openDevTools();
+    
+    
+    // openninng dev tools 
+    // mainWindow.openDevTools();
+
+
     // load html in window 
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname,'mainWindow.html'),
@@ -29,31 +35,41 @@ app.on('ready',function(){
         app.quit();
     })
 
+    mainWindow.on('resize', function () {
+        var size   = mainWindow.getSize();
+        var width  = size[0];
+        var height = size[1];
+        console.log("width: " + width);
+        console.log("height: " + height);
+        if (json_data_loaded){
+            mainWindow.webContents.send('resize', 'resize')
+        }
+    });
+
     //build menu from tempalte
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // insert menu 
     Menu.setApplicationMenu(mainMenu);
 
-
 });
 
 // handle new windows 
-function createAddWindow(){
+// function createAddWindow(){
 
-        //create new window 
-        addWindow = new BrowserWindow({
-            width: 300, 
-            height: 200, 
-            title: 'Add shopping list item'
-        });
-        // load html in window 
-        addWindow.loadURL(url.format({
-            pathname: path.join(__dirname,'addWindow.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
+//         //create new window 
+//         addWindow = new BrowserWindow({
+//             width: 300, 
+//             height: 200, 
+//             title: 'Add shopping list item'
+//         });
+//         // load html in window 
+//         addWindow.loadURL(url.format({
+//             pathname: path.join(__dirname,'addWindow.html'),
+//             protocol: 'file:',
+//             slashes: true
+//         }));
 
-}
+// }
 
 function find_values_by_key(json_data, target_key, partial = false) {
     output = []
@@ -78,104 +94,6 @@ function find_values_by_key(json_data, target_key, partial = false) {
     return output; 
 } 
 
-
-// // this function is only used for aces, and unique children will be used to find the parent
-// function find_parent_of_key(json_data, target_key) {
-//     output = []
-//     for (var key_ in json_data) {
-//         if (key_ == target_key){
-//             output = output.concat([json_data[key_]]);
-
-//         }
-//         else if (typeof (json_data[key_]) == 'object') {
-//             output = output.concat(find_values_by_key(json_data[key_], target_key, partial=false));
-//     }
-//     return output; 
-// } 
-
-
-function mud_to_nodes(multi_mud_json){
-    nodes = []
-    links = []
-    modelnames = find_values_by_key(multi_mud_json,"model-name");
-    unique_modelnames = [... new Set(modelnames)];
-    
-    // group_counter = new Array(unique_modelnames.length).fill(1)
-    
-    nodes.push({"group":"2","id":"Router"})
-    nodes.push({"group":"3","id":"Internet"})
-    links.push({"source": "Router","target":"Internet","value": "10"})  
-
-    
-    // take out the non-unique values 
-    var sorted_models = modelnames.slice().sort();
-    var non_unique_models = [];
-
-    for (var i = 0; i < sorted_models.length - 1; i++) {
-        if (sorted_models[i + 1] == sorted_models[i]) {
-            non_unique_models.push([sorted_models[i],1]);
-        }
-    }
-
-    // iterate through each mud file
-    for (var current_mud_name in  multi_mud_json){
-        var current_mud = multi_mud_json[current_mud_name];
-        // tmp = new Mud(current_mud);
-        model = find_values_by_key(current_mud,'model-name')[0]; 
-        for (var z = 0 ; z < non_unique_models.length; z++ ){
-            if (non_unique_models[z][0] == model){
-                model = model + non_unique_models[z][1]; 
-                non_unique_models[z][1] += 1; 
-                break;
-            }          
-        }
-
-        
-        var from_device_policies = find_values_by_key(find_values_by_key(current_mud,"from-device-policy"), "name");
-        var to_device_policies = find_values_by_key(find_values_by_key(current_mud,"to-device-policy"), "name");
-    
-        var link_of_current_node = [] ; 
-        link_of_current_node.push({"source": "Router","target":"Internet","value": "10"});
-        var all_acl_lists = find_values_by_key(current_mud,"ietf-access-control-list:acls");
-        var acls = []
-        for (var acl_idx=0; acl_idx < all_acl_lists.length ; acl_idx++){
-            acls = acls.concat(find_values_by_key(all_acl_lists[acl_idx],'acl')[0]);
-        }
-        for (var acl_i = 0 ; acl_i < acls.length ; acl_i++){
-            // check if the name of that ACL exists in the acl rule 
-            var acl_found = false;
-            for (var ii = 0; ii < from_device_policies.length; ii++) {
-                tmp_acl_names = find_values_by_key( acls[acl_i],'name'); 
-                if (tmp_acl_names.indexOf(from_device_policies[ii]) > -1) {
-                    acl_found = true;
-                    break;
-                }
-            }
-    
-            if (acl_found == true) {
-                target =  find_values_by_key(acls[acl_idx],"ietf-acldns:dst-dnsname") [0]; 
-                // check if node is already added
-                if (find_values_by_key(Object.values(nodes),'id').indexOf(target) == -1) {
-                nodes.push({"group":String(4),"id":target})
-                }
-                // check if link is already added 
-                if (find_values_by_key(Object.values(links),'source').indexOf(model) == -1  ||  
-                    find_values_by_key(Object.values(links),'target').indexOf(target) == -1){
-                links.push({"source": model,"target":"Router","value": "10"});
-                link_of_current_node.push({"source": model,"target":"Router","value": "10"})
-
-                links.push({"source": "Internet","target":target,"value": "10"});  
-                link_of_current_node.push({"source": "Internet","target":target,"value": "10"});  
-                }
-            }
-            
-        }
-        nodes.push({"group":String(1),"id":model, "links":link_of_current_node});
-    }
-    
-    return {"nodes": nodes, "links": links};
-  }
-
 // create menu tempalte 
 const mainMenuTemplate = [
     {
@@ -186,7 +104,7 @@ const mainMenuTemplate = [
                 label: 'Open',
                 accelerator: process.platform == 'darwin' ? 'Command+O' : 'Ctrl+O',
                 click(){
-                    const { dialog } = require('electron')
+                    const { dialog } = require('electron');
                     dialog.showOpenDialog((fileNames) => {
                         // fileNames is an array that contains all the selected
                         if(fileNames === undefined){
@@ -202,20 +120,21 @@ const mainMenuTemplate = [
                             }
                             
                             // Change how to handle the file content
-                            console.log("The file content is : " + data);
+                            // console.log("The file content is : " + data);
                             
-                            test_network = new Mud_Network(JSON.parse(data));
-                            test_network.create_network()
-                            the_data = test_network.get_nodes_links_json();
+                            network = new Mud_Network(JSON.parse(data));
+                            network.create_network()
+                            network_data = network.get_nodes_links_json();
 
                             // global.sharedObj = JSON.stringify(mud_to_nodes(JSON.parse(data))); 
-                            global.sharedObj = JSON.stringify(the_data); 
-                            console.log("############### The file content is : " + global.sharedObj);
-                            mainWindow.webContents.send('ping', 'whoooooooh!')
+                            global.sharedObj = JSON.stringify(network_data); 
+                            // console.log("############### The file content is : " + global.sharedObj);
+                            mainWindow.webContents.send('draw', 'draw')
                         });
                         
                         
                     });
+                    json_data_loaded = true; 
                 }
             },
             {
--- mud_visualizer/renderer.js
+++ mud_visualizer/renderer.js
@@ -5,142 +5,152 @@
 // ====================
 // READING OF JSON FILE 
 // ====================
-function readTextFile(file, callback) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/json");
-  rawFile.open("GET", file, true);
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
-      callback(rawFile.responseText);
-    }
-  }
-  rawFile.send(null);
-}
+// function readTextFile(file, callback) {
+//   var rawFile = new XMLHttpRequest();
+//   rawFile.overrideMimeType("application/json");
+//   rawFile.open("GET", file, true);
+//   rawFile.onreadystatechange = function () {
+//     if (rawFile.readyState === 4 && rawFile.status == "200") {
+//       callback(rawFile.responseText);
+//     }
+//   }
+//   rawFile.send(null);
+// }
 
-function OnClickDetails(deviceid) {
-  //alert("devicedetails: " + deviceid);
-  //usage:
+// function OnClickDetails(deviceid) {
+//   //alert("devicedetails: " + deviceid);
+//   //usage:
 
-  // #############################
-  // # READING NEIGHBORS         #
-  // #############################  
-  readTextFile("python/neighborships.json", function (text) {
-    var data = JSON.parse(text);
-    console.log(data);
-    console.log(deviceid);
+//   // #############################
+//   // # READING NEIGHBORS         #
+//   // #############################  
+//   readTextFile("python/neighborships.json", function (text) {
+//     var data = JSON.parse(text);
+//     console.log(data);
+//     console.log(deviceid);
 
-    bFoundMatch = 0;
-    for (var key in data) {
-      console.log("Key: " + key + " vs " + deviceid);
+//     bFoundMatch = 0;
+//     for (var key in data) {
+//       console.log("Key: " + key + " vs " + deviceid);
 
-      if ((deviceid.localeCompare(key)) == 0) {
-        console.log("match!");
-        bFoundMatch = 1;
-        text = tableFromNeighbor(key, data);
+//       if ((deviceid.localeCompare(key)) == 0) {
+//         console.log("match!");
+//         bFoundMatch = 1;
+//         text = tableFromNeighbor(key, data);
 
-        printToDivWithID("infobox", "<h2><u>" + key + "</u></h2>" + text);
-      }
-    }
-    if (!(bFoundMatch)) {
-      warning_text = "<h4>The selected device id: ";
-      warning_text += deviceid;
-      warning_text += " is not in database!</h4>";
-      warning_text += "This is most probably as you clicked on edge node ";
-      warning_text += "that is not NETCONF data gathered, try clicking on its neighbors.";
-      printToDivWithID("infobox", warning_text);
-    }
-  });
+//         printToDivWithID("infobox", "<h2><u>" + key + "</u></h2>" + text);
+//       }
+//     }
+//     if (!(bFoundMatch)) {
+//       warning_text = "<h4>The selected device id: ";
+//       warning_text += deviceid;
+//       warning_text += " is not in database!</h4>";
+//       warning_text += "This is most probably as you clicked on edge node ";
+//       warning_text += "that is not NETCONF data gathered, try clicking on its neighbors.";
+//       printToDivWithID("infobox", warning_text);
+//     }
+//   });
 
-  // ####################################
-  // # READING NEIGHBOR-LESS INTERFACES #
-  // ####################################
-  readTextFile("python/no_neighbor_interfaces.json", function (text) {
-    var data = JSON.parse(text);
-    console.log(data);
-    console.log(deviceid);
+//   // ####################################
+//   // # READING NEIGHBOR-LESS INTERFACES #
+//   // ####################################
+//   readTextFile("python/no_neighbor_interfaces.json", function (text) {
+//     var data = JSON.parse(text);
+//     console.log(data);
+//     console.log(deviceid);
 
-    bFoundMatch = 0;
-    for (var key in data) {
-      console.log("Key: " + key + " vs " + deviceid);
+//     bFoundMatch = 0;
+//     for (var key in data) {
+//       console.log("Key: " + key + " vs " + deviceid);
 
-      if ((deviceid.localeCompare(key)) == 0) {
-        console.log("match!");
-        bFoundMatch = 1;
-        text = tableFromUnusedInterfaces(key, data);
-        printToDivWithID("infobox2", "<font color=\"red\">Enabled Interfaces without LLDP Neighbor:</font><br>" + text);
-      }
-    }
-    if (!(bFoundMatch)) {
-      printToDivWithID("infobox2", "");
-    }
-  });
-}
-
-// ####################################
-// # using input parameters returns 
-// # HTML table with these inputs
-// ####################################
-function tableFromUnusedInterfaces(key, data) {
-  text = "<table class=\"infobox2\">";
-  text += "<thead><th><u><h4>LOCAL INT.</h4></u></th><th><u><h4>DESCRIPTION</h4></u></th><th><u><h4>Bandwith</h4></u></th>";
-  text += "</thead>";
-
-  for (var neighbor in data[key]) {
-    text += "<tr>";
-
-    console.log("local_intf:" + data[key][neighbor]['local_intf']);
-    text += "<td>" + data[key][neighbor]['local_intf'] + "</td>";
-    console.log("description:" + data[key][neighbor]['description']);
-    text += "<td>" + data[key][neighbor]['description'] + "</td>";
-    console.log("actual_bandwith:" + data[key][neighbor]['actual_bandwith']);
-    text += "<td>" + data[key][neighbor]['actual_bandwith'] + "</td>";
-
-    text += "</tr>";
-  }
-
-  text += "</table>";
-
-  return text;
-}
+//       if ((deviceid.localeCompare(key)) == 0) {
+//         console.log("match!");
+//         bFoundMatch = 1;
+//         text = tableFromUnusedInterfaces(key, data);
+//         printToDivWithID("infobox2", "<font color=\"red\">Enabled Interfaces without LLDP Neighbor:</font><br>" + text);
+//       }
+//     }
+//     if (!(bFoundMatch)) {
+//       printToDivWithID("infobox2", "");
+//     }
+//   });
+// }
 
 // ####################################
 // # using input parameters returns 
 // # HTML table with these inputs
 // ####################################
-function tableFromNeighbor(key, data) {
-  text = "<table class=\"infobox\">";
-  text += "<thead><th><u><h4>LOCAL INT.</h4></u></th><th><u><h4>NEIGHBOR</h4></u></th><th><u><h4>NEIGHBOR'S INT</h4></u></th>";
-  text += "</thead>";
+// function tableFromUnusedInterfaces(key, data) {
+//   text = "<table class=\"infobox2\">";
+//   text += "<thead><th><u><h4>LOCAL INT.</h4></u></th><th><u><h4>DESCRIPTION</h4></u></th><th><u><h4>Bandwith</h4></u></th>";
+//   text += "</thead>";
 
-  for (var neighbor in data[key]) {
-    text += "<tr>";
+//   for (var neighbor in data[key]) {
+//     text += "<tr>";
 
-    console.log("local_intf:" + data[key][neighbor]['local_intf']);
-    text += "<td>" + data[key][neighbor]['local_intf'] + "</td>";
-    console.log("neighbor_intf:" + data[key][neighbor]['neighbor_intf']);
-    text += "<td>" + data[key][neighbor]['neighbor'] + "</td>";
-    console.log("neighbor:" + data[key][neighbor]['neighbor']);
-    text += "<td>" + data[key][neighbor]['neighbor_intf'] + "</td>";
+//     console.log("local_intf:" + data[key][neighbor]['local_intf']);
+//     text += "<td>" + data[key][neighbor]['local_intf'] + "</td>";
+//     console.log("description:" + data[key][neighbor]['description']);
+//     text += "<td>" + data[key][neighbor]['description'] + "</td>";
+//     console.log("actual_bandwith:" + data[key][neighbor]['actual_bandwith']);
+//     text += "<td>" + data[key][neighbor]['actual_bandwith'] + "</td>";
 
-    text += "</tr>";
-  }
+//     text += "</tr>";
+//   }
 
-  text += "</table>";
+//   text += "</table>";
 
-  return text;
-}
+//   return text;
+// }
+
+// ####################################
+// # using input parameters returns 
+// # HTML table with these inputs
+// ####################################
+// function tableFromNeighbor(key, data) {
+//   text = "<table class=\"infobox\">";
+//   text += "<thead><th><u><h4>LOCAL INT.</h4></u></th><th><u><h4>NEIGHBOR</h4></u></th><th><u><h4>NEIGHBOR'S INT</h4></u></th>";
+//   text += "</thead>";
+
+//   for (var neighbor in data[key]) {
+//     text += "<tr>";
+
+//     console.log("local_intf:" + data[key][neighbor]['local_intf']);
+//     text += "<td>" + data[key][neighbor]['local_intf'] + "</td>";
+//     console.log("neighbor_intf:" + data[key][neighbor]['neighbor_intf']);
+//     text += "<td>" + data[key][neighbor]['neighbor'] + "</td>";
+//     console.log("neighbor:" + data[key][neighbor]['neighbor']);
+//     text += "<td>" + data[key][neighbor]['neighbor_intf'] + "</td>";
+
+//     text += "</tr>";
+//   }
+
+//   text += "</table>";
+
+//   return text;
+// }
 
 // ####################################
 // # replaces content of specified DIV
 // ####################################
-function printToDivWithID(id, text) {
-  div = document.getElementById(id);
-  div.innerHTML = text;
-}
+// function printToDivWithID(id, text) {
+//   div = document.getElementById(id);
+//   div.innerHTML = text;
+// }
 
 // ########
 // # MAIN #
 // ########
+// var svg = d3.select("svg"),
+//   //width = +svg.attr("width"),
+//   //height = +svg.attr("height");
+//   width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+//   height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+// d3.select("svg").attr("height", height)
+// d3.select("svg").attr("width", width)
+  
+
+
 var svg = d3.select("svg"),
   //width = +svg.attr("width"),
   //height = +svg.attr("height");
@@ -148,7 +158,7 @@ var svg = d3.select("svg"),
   height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 d3.select("svg").attr("height", height)
-d3.select("svg").attr("width", width * 0.7)
+d3.select("svg").attr("width", width)
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
@@ -157,24 +167,31 @@ var simulation = d3.forceSimulation()
   .force("charge", d3.forceManyBody().strength(-200).distanceMax(500).distanceMin(50))
   .force("x", d3.forceX(function (d) {
     if (d.group === "1") {
-      return 3 * (width * 0.7) / 4
+      return 4 * (width) / 5
     } else if (d.group === "2") {
-      return 2 * (width * 0.7) / 4
+      return 3 * (width) / 5
     } else if (d.group === "3") {
-      return 1 * (width * 0.7) / 4
+      return 2 * (width) / 5
+    } else if (d.group === "4") {
+      return 1 * (width) / 5
     } else {
-      return 0 * (width * 0.7) / 4
+      return 0 * (width) / 5
     }
   }).strength(1))
   .force("y", d3.forceY(height / 2))
-  .force("center", d3.forceCenter((width * 0.7) / 2, height / 2))
+  // .force("center", d3.forceCenter((width) / 2, height / 2))
   .force("collision", d3.forceCollide().radius(35));
 
 // ######################################
 // # Read graph.json and draw SVG graph #
 // ######################################
+
 function mud_drawer (){
-  
+
+  var oig_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var orig_height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+
   link_color = "black";
   link_hover_color = "green";
   var remote = require('electron').remote;
@@ -193,17 +210,12 @@ function mud_drawer (){
     };
 
     var link = svg.append("g")
-      // .selectAll("line")
       .selectAll("path")
       .data(graph.links)
       .enter().append("svg:path")
-      // .attr("stroke", function (d) { return color(parseInt(d.value)); })
-      // .attr("stroke-width", function (d) { return Math.sqrt(parseInt(d.value)); })
       .attr("fill","none")
-      // .attr("stroke", function (d) { return color(parseInt(d.value)); })
       .attr("stroke",link_color)
       .attr("stroke-width", function (d) { return Math.sqrt(parseInt(1)); })
-      // .attr("stroke-dasharray","0,2 1")
       .attr("src",function(d){ return d.source;})
       .attr("trg",function(d){return d.target;})
       .attr("dev",function(d){return d.device;})
@@ -218,7 +230,7 @@ function mud_drawer (){
       .enter().append("a")
       .attr("target", '_blank')
       .attr("xlink:href", function (d) { return (window.location.href + '?device=' + d.id) });
-      //
+
 
     node.on("click", function (d, i) {
       d3.event.preventDefault();
@@ -259,44 +271,44 @@ function mud_drawer (){
       .links(graph.links);
 
     function ticked() {
-      // link
-        // .attr("x1", function (d) { return d.source.x ; })
-        // .attr("y1", function (d) { return d.source.y; })
-        // .attr("x2", function (d) { return d.target.x; })
-        // .attr("y2", function (d) { return d.target.y; });
 
-      link.attr("d", function(d) {
-        var dx = d.target.x - d.source.x,
-            dy = d.target.y - d.source.y,
-            dr = 400/d.linknum;  //linknum is defined above
-        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;});
+      // link.attr("d", function(d) {
+      //   var dx = d.target.x - d.source.x,
+      //       dy = d.target.y - d.source.y,
+      //       dr = 400/d.linknum;  //linknum is defined above
+      //   return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;});
+      
+      link.attr("d", linkArc);
 
       node
         .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")" });
     }
+    
+    function linkArc(d) {
+      var dx = d.target.x - d.source.x,
+          dy = d.target.y - d.source.y,
+          dr = oig_width/d.linknum;
+      return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+    }
+
+
     simulation.alphaTarget(0.3).restart();
     
-
-    // node.on("mouseover",function(d){d3.select(d.links).style("stroke","pink");});
     node.on("mouseover",function(d){ 
       var current_node_links = d.links ; 
       var current_device = d.id 
       d3.selectAll('path').each(function(d,i){ 
-        // console.log("#####"  + JSON.stringify(current_node_links[0]) );
         for (var ll = 0; ll < current_node_links.length ; ll ++ ){
           if (d3.select(this).attr("src") == current_node_links[ll]["source"] && 
               d3.select(this).attr("trg") == current_node_links[ll]["target"] && 
               d3.select(this).attr("dev") == current_device 
               ){
-            // console.log("hi " + d3.select(this).attr("src"));
             d3.select(this)
               .style("stroke",link_hover_color)
               .style("stroke-width", 2);
 
             
               totalLength = 10;
-              // repeat();
-              // function repeat() {
                 d3.select(this)
                 .attr("stroke-dasharray", totalLength + " " + totalLength/2)
                 .attr("stroke-dashoffset", totalLength*30)
@@ -304,21 +316,16 @@ function mud_drawer (){
                   .duration(10000)
                   .ease(d3.easeLinear)
                   .attr("stroke-dashoffset", 0)
-                  // .on("end", repeat);
-              // }
             }
           }    
         }
         )
-      // d3.selectAll('g.link').each(function(){d3.select(this).style("stroke","pink");});
     });
     node.on("mouseout",function(d){ 
       var current_node_links = d.links ; 
       d3.selectAll('path').each(function(d,i){ 
-        // console.log("#####"  + JSON.stringify(current_node_links[0]) );
         for (var ll = 0; ll < current_node_links.length ; ll ++ ){
           if (d3.select(this).attr("src") == current_node_links[ll]["source"] && d3.select(this).attr("trg") == current_node_links[ll]["target"] ){
-            // console.log("hi " + d3.select(this).attr("src"));
             d3.select(this)
               .style("stroke",link_color)
               .style("stroke-width", 1);
@@ -337,7 +344,7 @@ function mud_drawer (){
     link.on("mouseover", function(){ d3.select(this).style("stroke",link_hover_color);} );
     link.on("mouseout", function(){ d3.select(this).style("stroke",link_color)});
   
-}
+
 
 
 function dragstarted(d) {
@@ -353,10 +360,24 @@ function dragged(d) {
 
 function dragended(d) {
   if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
+  // d.fx = null;
+  // d.fy = null;
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
 
-require('electron').ipcRenderer.on('ping', (event, message) => {
+}
+}
+require('electron').ipcRenderer.on('draw', (event, message) => {
   mud_drawer();
+})
+
+require('electron').ipcRenderer.on('resize', (event, message) => {
+  
+  width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+  height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  d3.select("svg").attr("height", height)
+  d3.select("svg").attr("width", width)
+  // to redraw : 
+  // d3.selectAll("svg > *").remove();
+  // mud_drawer();
 })
